4ae87fcd9a93e2e8312453f5fe8ea24064c42c57
Lucas <lucaspoliveira822@gmail.com>
2025-11-03 11:27:07 -0300

    Initial commit

---
+++ src/main/java/com/medical/medservice/MedserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medical.medservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedserviceApplication.class, args);
+	}
+
+}
